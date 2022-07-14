db34cdcab6ae3093cd9dd63ab72ab2afd2ecc71f
Vesna Tan <tanvezi@gmail.com>
2022-07-14 16:39:23 -0400

    new branch due to conflicting upstream code changes

---
+++ catchpy/settings/docker-entrypoint.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+./python_venv/bin/python manage.py migrate                  # Apply database migrations
+./python_venv/bin/python manage.py collectstatic --noinput  # Collect static files
+
+# Start Gunicorn processes
+echo Starting Gunicorn.
+exec ./python_venv/bin/gunicorn -c catchpy/settings/gunicorn.conf.py catchpy.wsgi:application 
